58f484e73008cc26cf268871ad076b9b44fb1ee7
Василий Пупкин < >
2016-12-10 22:05:05 +0300

    Updated code, etc...

--- analyze.py
+++ analyze.py
@@ -1,41 +1,96 @@
-import numpy
-import pandas
+import numpy as np
+import pandas as pd
 import matplotlib
 import sqlalchemy as sqlalc
+from functools import *
 
 engine = sqlalc.create_engine('mysql://ght:@127.0.0.1/ghtorrent')
+
+users_data = {}
+users_languages = {}
+projects_datas = []
+projects_users = []
+
+#from http://stackoverflow.com/questions/34411495/pandas-merge-several-dataframes
+def my_merge(dfs):
+    cols, rows = [], []
+    for df_i in dfs:
+        cols = cols + df_i.columns.tolist()
+        rows = rows + df_i.index.tolist()
+    cols = np.unique(cols)
+    rows = np.unique(rows)       
+    df = pd.DataFrame(data=np.NaN, columns=cols, index=rows) 
+
+    for df_i in dfs:
+        df.loc[df_i.index, df_i.columns] = df_i.values
+    return df
+
+def get_user_name(user_id):
+    global engine
+    query = "SELECT login FROM users WHERE id=%s LIMIT 1 ;"
+    data = pd.read_sql(query, engine, params = [user_id])
+    if len(data) == 0:
+        return "~INVALID_USER"
+    return data.login[0]
 
 def get_user_id(user_name):
     global engine
     query = "SELECT p.owner_id FROM projects p JOIN users u ON u.id=p.owner_id WHERE u.login=%s LIMIT 1 ;"
-    data = pandas.read_sql(query, engine, params = [user_name])
+    data = pd.read_sql(query, engine, params = [user_name])
+    if len(data) == 0:
+        return "-1"
     return data.owner_id[0]
-
-users_data = {}
 
 def load_user(user_name):
     global engine
+    global users_data
+    
+    if user_name in users_data:
+        return
     query = "SELECT id,login,company,location,created_at,type,fake,deleted,country_code,state,city FROM users WHERE login=%s LIMIT 1 ;"
-    data = pandas.read_sql(query, engine, params = [user_name])
+    data = pd.read_sql(query, engine, params = [user_name])
+    
+    if len(data) == 0:
+        return
+    
     users_data[user_name] = data
+    users_languages[user_name] = set()
 
 def load_projects_by(user_name):
     global engine
+    global projects_datas
+    global users_data
+    global users_languages
+    
+    if not user_name in users_data:
+        load_user(user_name)
+    
     query = "SELECT p.*, u.login FROM projects p JOIN users u ON u.id=p.owner_id WHERE u.login=%s ;"
-    print(query % user_name)
-    data = pandas.read_sql(query, engine, params = [user_name])
-    print(data)
+    data = pd.read_sql(query, engine, params = [user_name])
+    
+    if len(data) == 0:
+        return
+    
+    for idx, row in data.iterrows():
+        users_languages[user_name].add(row.language)
+    
+    projects_datas += [data]
 
 #get_user_id("Vany")
 
-#query = "DESCRIBE users ;"
-#data = pandas.read_sql(query, engine)
-#print(data)
+user_id_start = get_user_id("Vany")
+for user_id in range(user_id_start, user_id_start + 10):
+    user_name = get_user_name(user_id)
+    if user_name == "~INVALID_USER":
+        continue
+    load_user(user_name)
+    load_projects_by(user_name)
 
-load_user("Vany")
-load_projects_by("Vany")
+projects_data = my_merge(projects_datas)
 
-#data = pandas.read_sql('SELECT * FROM `projects` LIMIT 100;', engine)
-#query = 'SELECT * FROM projects WHERE url LIKE "https://github.com/' + "Vany" + '/%" ;'
-#print(query)
-#print(data)
+print("!")
+print(projects_datas)
+print("!")
+print(projects_data)
+print("!")
+print(users_languages)
