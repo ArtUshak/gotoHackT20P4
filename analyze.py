615a09719dc580daad9f708ba38e534fe1d87200
???°?????»???? ???????????? <???????????? ????????????>
2016-12-10 10:58:17 +0300

    Added analyze.py

---
+++ analyze.py
@@ -0,0 +1,13 @@
+import pandas
+import numpy
+import matplotlib as plt
+
+data_filename = 'data.csv'
+
+def load_structure(course_id):
+    return pandas.read_csv('data_stepic/course-' + str(course_id) + '-structure.csv')
+
+def load_events(course_id):
+    return pandas.read_csv('data_stepic/course-' + str(course_id) + '-events.csv')
+
+events = load_events(7)
