bbb627a0164b67ba1e650fab613dfe4d4f9d60bf
Василий Пупкин < >
2016-12-10 11:08:07 +0300

    Fixed path to data files

--- analyze.py
+++ analyze.py
@@ -5,9 +5,9 @@ import matplotlib as plt
 data_filename = 'data.csv'
 
 def load_structure(course_id):
-    return pandas.read_csv('data_stepic/course-' + str(course_id) + '-structure.csv')
+    return pandas.read_csv('../data_stepic/course-' + str(course_id) + '-structure.csv')
 
 def load_events(course_id):
-    return pandas.read_csv('data_stepic/course-' + str(course_id) + '-events.csv')
+    return pandas.read_csv('../data_stepic/course-' + str(course_id) + '-events.csv')
 
 events = load_events(7)
